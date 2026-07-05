bd666196be3be60667d526d1c295ac34fffefd6d
Zeros-CodeXD <ayanc8261@gmail.com>
2026-07-05 18:39:32 +0530

    chore: add trailing slash to base configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zeros-codexd.github.io',
-  base: '/Portfolio-website',
+  base: '/Portfolio-website/',
   vite: {
     plugins: [tailwindcss()],
   },
